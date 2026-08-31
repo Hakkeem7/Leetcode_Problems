336b32aa6375c6ccff9fab2a9257f0d4e91d9acc
K.ABDUL HAKKEEM <abdulhakkeem137@gmail.com>
2026-08-31 07:26:01 +0100

    Time: 39 ms (94.55%), Space: 20 MB (88.46%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -13,10 +13,10 @@ class Solution:
         if not head:
             return None
         
-        # Dictionary: original node -> copied node
+        
         old_to_new = {}
         
-        # Pass 1: Create all copied nodes
+    
         current = head
         
         while current:
@@ -24,18 +24,15 @@ class Solution:
             current = current.next
         
         
-        # Pass 2: Connect next and random pointers
         current = head
         
         while current:
             
             copy_node = old_to_new[current]
             
-            # Connect next
             if current.next:
                 copy_node.next = old_to_new[current.next]
             
-            # Connect random
             if current.random:
                 copy_node.random = old_to_new[current.random]
             
