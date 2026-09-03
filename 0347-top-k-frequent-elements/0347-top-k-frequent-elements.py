1c9e688882890409ca6fc6a106a4025b7d99eb3e
K.ABDUL HAKKEEM <abdulhakkeem137@gmail.com>
2026-09-03 06:38:23 +0100

    Time: 4 ms (68.99%), Space: 22.7 MB (80.66%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,21 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        import heapq
+        # Step 1: Count frequencies
+        freq = {}
+
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
+        # Step 2: Keep only K most frequent elements
+        heap = []
+
+        for num, count in freq.items():
+
+            heapq.heappush(heap, (count, num))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        # Step 3: Extract numbers
+        return [num for count, num in heap]
